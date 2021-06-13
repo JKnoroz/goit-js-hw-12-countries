da2bc880dd76e1892e2d01d5fb66a1d56826bfd1
Yuliya <jknoroz@gmail.com>
2021-06-13 17:48:29 +0300

    3

--- src/js/input.js
+++ src/js/input.js
@@ -1,33 +1,22 @@
-// import countryCardTpl from '../partials/countries-list.hbs';
-// import countriesListTpl from '';
-
 import _debounce from 'lodash.debounce';
 import countryCardTpl from '../partials/country-card.hbs';
+import API from '../js/fetchCountries';
 
 const input = document.querySelector('.country-input');
-console.log(input);
+const cardContainer = document.querySelector('.js-country');
 
 input.addEventListener('input', _debounce(onSearch, 500));
 
-function onSearch(e) {
+function onSearch() {
   const searchQuery = input.value;
-
-  fetchCountryByName(searchQuery)
-    .then(renderCountryCard)
-    .catch(error => {
-      console.log(error);
-    });
-}
-
-const cardContainer = document.querySelector('.js-country');
-
-function fetchCountryByName(name) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(response => {
-    return response.json();
-  });
+  API.fetchCountryByName(searchQuery).then(renderCountryCard).catch(onFetchError);
 }
 
 function renderCountryCard(country) {
   const markup = countryCardTpl(country);
   cardContainer.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  alert('WTF');
 }
